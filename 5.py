2ca8f8f6b3547678546d7c11544c500b947b2342
HoangMinh153 <=>
2021-06-16 10:51:58 +0700

    add 5.py

---
+++ 5.py
@@ -0,0 +1,9 @@
+a,b=map(int,input().split())
+
+for i in range(1,a//2 + 1):
+    print(('.|.'*(2*i-1)).center(b,'-'))
+print('WELCOME'.center(b,'-'))
+for i in range(a//2  ,0,-1):
+    print(('.|.'*(2*i-1)).center(b,'-'))   
+        
+    
