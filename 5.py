0236ecd8fcd645298bd91e90257d5398e613b29e
HoangMinh153 <=>
2021-06-18 15:15:29 +0700

    thank u

--- 5.py
+++ 5.py
@@ -4,6 +4,8 @@ for i in range(1,a//2 + 1):
     print(('.|.'*(2*i-1)).center(b,'-'))
 print('WELCOME'.center(b,'-'))
 for i in range(a//2  ,0,-1):
-    print(('.|.'*(2*i-1)).center(b,'-'))   
+    print(('.|.'*(2*i-1)).center(b,'-'))
+print('thank u')
+
         
     
