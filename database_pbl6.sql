620de908e97fbc7018ba39d61560562c73d50883
HoangMinh153 <84728492+HoangMinh153@users.noreply.github.com>
2023-09-16 22:43:39 +0700

    feat(): update database and change name main2 to face_recognition

--- database_pbl6.sql
+++ database_pbl6.sql
@@ -1,64 +1,70 @@
 CREATE TABLE IF NOT EXISTS Users (
-  Account TEXT(255),
-  Address TEXT(255),
-  Mail TEXT(255),
-  Name TEXT(255),
-  Password TEXT(255),
-  Phone_number TEXT(255),
+  Account VARCHAR(255),
+  Address VARCHAR(255),
+  Mail VARCHAR(255),
+  Name VARCHAR(255),
+  Password VARCHAR(255),
+  Phone_number VARCHAR(255),
   User_id INT PRIMARY KEY AUTO_INCREMENT
 );
 
 CREATE TABLE IF NOT EXISTS Orders (
   Date_of_orders DATE,
   Order_id INT PRIMARY KEY AUTO_INCREMENT,
-  Total_price TEXT(255),
+  Supplier_id INT,
+  Total_price VARCHAR(255),
   User_id INT AUTO_INCREMENT
 );
+
+
 
 CREATE TABLE IF NOT EXISTS Order_Details (
   Number_of_products INT,
   Order_detail_id INT PRIMARY KEY AUTO_INCREMENT,
   Order_id  INT AUTO_INCREMENT,
-  Payment_methods TEXT(255),
+  Payment_methods VARCHAR(255),
   Price INT,
   Product_id  INT AUTO_INCREMENT,
-  QR TEXT(255),
+  QR VARCHAR(255),
   Status BOOLEAN
 );
 
 CREATE TABLE IF NOT EXISTS Admins (
-  Account TEXT(255),
+  Account VARCHAR(255),
   Admin_id INT PRIMARY KEY AUTO_INCREMENT,
-  Birth_day TEXT(255),
-  Name TEXT(255),
-  Password TEXT(255),
-  Role TEXT(255)
+  Birth_day VARCHAR(255),
+  Email VARCHAR(255),
+  Name VARCHAR(255),
+  Password VARCHAR(255),
+  Role VARCHAR(255),
+  Sex VARCHAR(255)
 );
-
 CREATE TABLE IF NOT EXISTS Products (
   Battery INT,
-  Camera TEXT(255),
-  CPU TEXT(255),
-  Description TEXT(255),
+  Brand_id INT,
+  Camera VARCHAR(255),
+  CPU VARCHAR(255),
+  Description VARCHAR(255),
   Discount BOOLEAN,
   Discount_percent INT,
-  Input TEXT(255),
-  Manufacturers TEXT(255),
+  Input VARCHAR(255),
+  Manufacturers VARCHAR(255),
   Memory INT,
-  Name TEXT(255),
-  Operating_system TEXT(255),
-  Output TEXT(255),
+  Name VARCHAR(255),
+  Operating_system VARCHAR(255),
+  Output VARCHAR(255),
   Price INT,
   Product_id INT PRIMARY KEY AUTO_INCREMENT,
   RAM INT,
-  Remaining_quantity TEXT(255),
-  Screen size TEXT(255),
-  Type TEXT(255)
+  Remaining_quantity VARCHAR(255),
+  Screen size VARCHAR(255),
+  Supplier_id INT,
+  Type VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS Chat (
   Admin_id INT,
-  chat_content TEXT(255),
+  Chat_content VARCHAR(255),
   Chat_id INT PRIMARY KEY AUTO_INCREMENT,
   Start_day DATE,
   Status BOOLEAN,
@@ -77,10 +83,35 @@ CREATE TABLE IF NOT EXISTS Data_Analysis (
   Total_amount INT
 );
 
+CREATE TABLE IF NOT EXISTS Ratings (
+  Created_date DATE,
+  Product_id INT,
+  Rating VARCHAR(255),
+  Rating_id INT PRIMARY KEY AUTO_INCREMENT,
+  Review VARCHAR(255),
+  User_id INT
+);
+
 CREATE TABLE IF NOT EXISTS Shopping_Cart (
   Cart_id INT PRIMARY KEY AUTO_INCREMENT,
   Order_id INT,
   Total_price INT,
   User_id INT
 );
+
+CREATE TABLE IF NOT EXISTS Suppliers (
+  Address VARCHAR(255),
+  Email VARCHAR(255),
+  Name VARCHAR(255),
+  Phone_number VARCHAR(255),
+  Supplier_id INT PRIMARY KEY AUTO_INCREMENT
+);
+
+CREATE TABLE IF NOT EXISTS Brands (
+  Brand_id INT PRIMARY KEY AUTO_INCREMENT,
+  Details VARCHAR(255),
+  Name VARCHAR(255)
+);
+
+
 
